19a05006be31b5aa7ec54d5a0fdeac7a388c9ece
Sahil Athia <sahilathia918@gmail.com>
2020-12-01 14:57:00 +0000

    update the auto loading feed of products

--- public/scripts/createListing.js
+++ public/scripts/createListing.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
+  loadProducts();
   $('#listing-slider').hide();
   $('#error-slider').hide();
 
   $('#create_listing').on('click', () => {
     $('#listing-slider').slideDown('slow');
-    visible = true;
   })
 
   $('#form-toggle').on('click', () => {
     $('#listing-slider').slideUp('slow');
+    $('#error-slider').hide();
     // clear the text area
   })
 
@@ -23,20 +24,25 @@ $(document).ready(function() {
       let product = {
         seller_id: text[0],
         name: text[1],
-        price: Number(text[2]),
+        price: Number(text[2] * 100),
         type: text[3],
         img_url: text[4],
         description: text[5]
       }
-      console.log('1')
+
       $.ajax({
         url: '/newProduct',
         method: "POST",
         data: product
       })
+      loadProducts();
       $('#listing-slider').slideUp('slow');
       loadProducts();
-
+      loadProducts();
+      loadProducts();
+      loadProducts();
+      loadProducts();
+      // preventitive measure since the website wont cooperate
     } else {
       $("#error-message").text('No Boxes Should Be Left Empty');
       $('#error-slider').slideDown('slow');
