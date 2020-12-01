fd4df22d4ccb33f3bb42675c7a0bb17183e8fbfc
Sahil Athia <sahilathia918@gmail.com>
2020-12-01 22:54:52 +0000

    add a user based favortie delete

--- routes/removeFromFavorites.js
+++ routes/removeFromFavorites.js
@@ -5,10 +5,10 @@ module.exports = (db) => {
   router.post("/", (request, response) => {
     const sqlQuery = `
       DELETE FROM user_favorites
-      WHERE user_id = 1
-      AND guitar_id = $1
+      WHERE user_id = (SELECT users.id FROM users WHERE email = $1)
+      AND guitar_id = $2;
     `
-    const values = [request.body.product_id]
+    const values = [request.session['user_id'], request.body.product_id]
     console.log('REMOVING')
     db.query(sqlQuery, values)
       .then(res => response.json(res.rows))
