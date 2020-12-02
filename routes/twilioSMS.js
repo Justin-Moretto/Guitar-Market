9c0be6b3f5a0ebd935db00442231b66b3a568a31
Jared <jaredflomen@gmail.com>
2020-12-02 19:02:57 +0000

    Add text input for twilio SMS

--- routes/twilioSMS.js
+++ routes/twilioSMS.js
@@ -1,15 +1,18 @@
+const express = require('express');
+const router  = express.Router();
+
+//Twilio API For SMS
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-const express = require('express');
-const router  = express.Router();
+
 
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
     client.messages
     .create({
-      body: 'Is this grogu?',
+      body: `${req.body['twilio-sms']}`,
       from: '+15013827815',
       to: '+14167271411'
       })
