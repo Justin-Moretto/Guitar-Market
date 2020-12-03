b5d03c61d104f516314ea47a635b11beccea137f
justin-moretto <JustinMorettoMTL@gmail.com>
2020-12-03 20:16:40 +0000

    added some listing to the guitasr seeds

--- db/seeds/02_guitars.sql
+++ db/seeds/02_guitars.sql
@@ -1,5 +1,8 @@
 INSERT INTO guitars (seller_id, name, price, type, img_url, description, sold)
   VALUES
-    (1, 'YAMAHA guitar', '30000', 'Acoustic', 'https://www.tomleemusic.ca/media/catalog/product/cache/image/1000x1320/e9c3970ab036de70892d86c6d221abfe/1/8/189161_1.jpg', 'Sunset blue acoustic guitar', FALSE),
-    (2, 'YAMAHA Pacifica', '45000', 'Electric', 'https://www.soundalchemy.com.sg/wp-content/uploads/2020/05/ymh_120h_blk.jpg', 'Black electric guitar', TRUE),
+    (1, 'Fender Stratocaster', '70000', 'Electric', 'https://images.reverb.com/image/upload/s--i_XZ_eEg--/f_auto,t_supersize/v1476392883/kdseiogfj88beyoxhejb.png', '2006 sunburst', FALSE),
+    (2, 'Gibson Les Paul', '95000', 'Electric', 'https://images.reverb.com/image/upload/s---8IYxzxO--/f_auto,t_supersize/v1567028341/vazsnj3cuvrkjrcvqddo.jpg', 'barely used', FALSE),
+    (3, 'Musicman Stingray', '85000', 'Bass', 'https://cdn.shopify.com/s/files/1/1304/0581/products/IMG_3637_c8ae9611-f666-4aa0-acfe-588b10e71822.jpg?v=1505507868', 'thicc sound', TRUE),
+    (2, 'YAMAHA guitar', '30000', 'Acoustic', 'https://www.tomleemusic.ca/media/catalog/product/cache/image/1000x1320/e9c3970ab036de70892d86c6d221abfe/1/8/189161_1.jpg', 'Sunset blue acoustic guitar', FALSE),
+    (1, 'YAMAHA Pacifica', '45000', 'Electric', 'https://www.soundalchemy.com.sg/wp-content/uploads/2020/05/ymh_120h_blk.jpg', 'Black electric guitar', TRUE),
     (3, 'HUNTINGTON guitar', '35000', 'Acoustic', 'https://m.media-amazon.com/images/I/713Seio-QnL._AC_SY606_.jpg', 'Red sunburst acoustic guitar', FALSE);
